4c89b328da955fe4374fa843bb4b967c65b96249
yl <yl@163.com>
2019-06-30 15:51:37 +0800

    suoyou

--- js/function.js
+++ js/function.js
@@ -1,9 +1,7 @@
 window.onload=function(){
-    var btn=document.querySelector("#btn")
-    var srt="这是master修改过的文件"
-<<<<<<< HEAD
-    var a="这是marster"
-=======
-    var b="这是zz"
->>>>>>> zz
+    var btn=document.querySelector("#btn");
+    var srt="这是master修改过的文件";
+    var a="这是marster";
+    var b="这是zz";
+    var c="123"
 }
